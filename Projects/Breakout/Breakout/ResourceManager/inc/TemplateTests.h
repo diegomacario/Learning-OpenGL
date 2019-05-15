4f65472b577b10e7d65588d9cae961777f63d9c6
diegomacario <diego.macario5@gmail.com>
2019-05-15 07:37:24 -0400

    Add function that prints the contents of a std::tuple

--- Projects/Breakout/Breakout/ResourceManager/inc/TemplateTests.h
+++ Projects/Breakout/Breakout/ResourceManager/inc/TemplateTests.h
@@ -2,7 +2,10 @@
 #define TEMPLATE_TESTS_H
 
 #include <tuple>
+#include <utility>
 #include <iostream>
+
+// -------------------------------------------------------------------------------
 
 template <class... Args>
 void createTuple(int i, Args... args)
@@ -11,11 +14,32 @@ void createTuple(int i, Args... args)
    std::tuple<Args...> argsTuple{args...};
 }
 
+// -------------------------------------------------------------------------------
+
 template <class... Args>
 void createTupleUniRef(int i, Args&&... args)
 {
    // Expand template parameter pack Args first, then function parameter pack args
    std::tuple<Args...> argsTuple{std::forward<Args>(args)...};
 }
+
+// -------------------------------------------------------------------------------
+
+template<std::size_t I = 0, typename... Tp>
+inline typename std::enable_if<I == sizeof...(Tp), void>::type
+printTuple(std::tuple<Tp...>& t)
+{
+   // End compile-time recursion
+}
+
+template<std::size_t I = 0, typename... Tp>
+inline typename std::enable_if<I < sizeof...(Tp), void>::type
+printTuple(std::tuple<Tp...>& t)
+{
+   std::cout << std::get<I>(t) << '\n';
+   printTuple<I + 1, Tp...>(t);
+}
+
+// -------------------------------------------------------------------------------
 
 #endif
--- Projects/Breakout/Breakout/ResourceManager/src/main.cpp
+++ Projects/Breakout/Breakout/ResourceManager/src/main.cpp
@@ -1,11 +1,16 @@
-#include <tuple>
-#include <iostream>
-
 #include "TemplateTests.h"
 
 int main()
 {
    createTuple(1, 2.0f, 3.0, 4u, "A");
+
    createTupleUniRef(1, 2.0f, 3.0, 4u, "A");
+
+   auto t = std::make_tuple(1, 2.0f, 3.0, 4u, "A");
+   printTuple(t);
+
+   int x;
+   std::cin >> x;
+
    return 0;
 }
