0856c483ca07dcbef143997296a5478752e57250
Diego Macario <diego.macario5@gmail.com>
2019-09-05 10:44:26 -0400

    Simplify the shader even more by removing the array of uniform structs

--- Projects/Breakout/Breakout/TeaPong/shaders/game_object_3D.fs
+++ Projects/Breakout/Breakout/TeaPong/shaders/game_object_3D.fs
@@ -7,18 +7,11 @@ in VertexData
    vec2 texCoords;
 } i;
 
-struct PointLight
-{
-   vec3  worldPos;
-   vec3  color;
-   float constantAtt;
-   float linearAtt;
-   float quadraticAtt;
-};
-
-#define MAX_NUMBER_OF_POINT_LIGHTS 4
-uniform PointLight pointLights[MAX_NUMBER_OF_POINT_LIGHTS];
-uniform int numPointLightsInScene;
+uniform vec3  pl_worldPos;
+uniform vec3  pl_color;
+uniform float pl_constantAtt;
+uniform float pl_linearAtt;
+uniform float pl_quadraticAtt;
 
 uniform vec3 cameraPos;
 
@@ -45,26 +38,22 @@ uniform float shininessConst;
 
 out vec4 fragColor;
 
-vec3 calculateContributionOfPointLight(PointLight light, vec3 viewDir);
+vec3 calculateContributionOfPointLight(vec3 viewDir);
 
 void main()
 {
    vec3 viewDir = normalize(cameraPos - i.worldPos);
 
-   vec3 color;
-   for(int i = 0; i < numPointLightsInScene; i++)
-   {
-      color += calculateContributionOfPointLight(pointLights[i], viewDir);
-   }
+   vec3 color = calculateContributionOfPointLight(viewDir);
 
    fragColor = vec4(color, 1.0);
 }
 
-vec3 calculateContributionOfPointLight(PointLight light, vec3 viewDir)
+vec3 calculateContributionOfPointLight(vec3 viewDir)
 {
    // Attenuation
-   float distance    = length(light.worldPos - i.worldPos);
-   float attenuation = 1.0 / (light.constantAtt + (light.linearAtt * distance) + (light.quadraticAtt * distance * distance));
+   float distance    = length(pl_worldPos - i.worldPos);
+   float attenuation = 1.0 / (pl_constantAtt + (pl_linearAtt * distance) + (pl_quadraticAtt * distance * distance));
 
    // Ambient
    // TODO: Do you really want the ambient light to be attenuated?
@@ -74,13 +63,13 @@ vec3 calculateContributionOfPointLight(PointLight light, vec3 viewDir)
    vec3 emissive     =   emissiveConst;
 
    // Diffuse
-   vec3  lightDir    =   normalize(light.worldPos - i.worldPos);
-   vec3  diff        =   max(dot(lightDir, i.worldNormal), 0.0) * light.color * attenuation;
+   vec3  lightDir    =   normalize(pl_worldPos - i.worldPos);
+   vec3  diff        =   max(dot(lightDir, i.worldNormal), 0.0) * pl_color * attenuation;
    vec3  diffuse     =   (diff * diffuseConst);
 
    // Specular
    vec3 reflectedDir =   reflect(-lightDir, i.worldNormal);
-   vec3 spec         =   pow(max(dot(reflectedDir, viewDir), 0.0), shininessConst) * light.color * attenuation;
+   vec3 spec         =   pow(max(dot(reflectedDir, viewDir), 0.0), shininessConst) * pl_color * attenuation;
    vec3 specular     =   (spec * specularConst);
 
    return (ambient + diffuse + specular + emissive);
